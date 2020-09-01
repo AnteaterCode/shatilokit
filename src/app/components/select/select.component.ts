bd2cc2e48107cc7f74212c2eccd97f475574cb41
ass51 <ass51@europlan.ru>
2020-09-01 18:18:29 +0300

    select timeline

---
+++ src/app/components/select/select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sh-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.scss']
+})
+export class SelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
