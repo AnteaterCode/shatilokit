e0e87e75341c7b69ce1d975130b2e303fc19a4e7
AnteaterKit <ashatilovdev@gmail.com>
2020-09-02 16:22:50 +0300

    paragraps

---
+++ src/stories/general/typography.stories.ts
@@ -0,0 +1,23 @@
+// also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/angular/types-6-0';
+
+import { moduleMetadata } from '@storybook/angular/dist/client/preview/types';
+
+const modules = {
+  imports: [
+  ],
+};
+
+export default {
+  title: 'general/Typography',
+} as Meta;
+
+
+export const fonts = () => ({
+});
+
+export const colors = () => ({
+});
+
+export const api = () => ({
+});
---
+++ src/stories/layout/layout.stories.ts
@@ -0,0 +1,17 @@
+// also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/angular/types-6-0';
+
+import { moduleMetadata } from '@storybook/angular/dist/client/preview/types';
+
+const modules = {
+  imports: [
+  ],
+};
+
+export default {
+  title: 'layout/Layout',
+} as Meta;
+
+
+export const layout = () => ({
+});
