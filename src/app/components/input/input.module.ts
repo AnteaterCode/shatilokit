c72b2702e08ff0a38ffe9124557f6e5df01621a9
ass51 <ass51@europlan.ru>
2020-09-15 15:07:00 +0300

    input

--- src/app/components/input/input.module.ts
+++ src/app/components/input/input.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShInputDirective } from './input.directive';
+import { ShInputFieldComponent } from './input-field.components';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ShInputDirective],
-  exports: [ShInputDirective]
+  declarations: [ShInputDirective, ShInputFieldComponent],
+  exports: [ShInputDirective, ShInputFieldComponent]
 })
 export class InputModule { }
